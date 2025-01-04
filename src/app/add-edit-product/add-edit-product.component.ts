e99cd8deadabd801af4bcf9f12f811cbf254612f
oussama <oukhayloussama@gmail.com>
2025-01-04 14:10:32 +0100

    update component

--- src/app/add-edit-product/add-edit-product.component.ts
+++ src/app/add-edit-product/add-edit-product.component.ts
@@ -1,12 +1,142 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ApiService} from "../service/api.service";
+
 
 @Component({
   selector: 'app-add-edit-product',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './add-edit-product.component.html',
   styleUrl: './add-edit-product.component.css'
 })
-export class AddEditProductComponent {
+export class AddEditProductComponent implements OnInit {
 
+
+  constructor(
+    private apiService: ApiService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+  }
+
+  productId: string | null = null;
+  name: string = '';
+  sku: string = '';
+  price: string = '';
+  stockQuantity: string = '';
+  categoryId: string = '';
+  description: string = '';
+  imageUrl: string = '';
+  imageFile: File | null = null;
+  isEditing: boolean = false;
+  categories: any = [];
+  message: string = '';
+
+  ngOnInit(): void {
+    this.productId = this.route.snapshot.paramMap.get('productId');
+    this.getCategories();
+    if (this.productId) {
+      this.isEditing = true;
+      this.getProductById(this.productId);
+    }
+  }
+
+  getCategories(): void {
+    this.apiService.getAllCategory().subscribe({
+      next: (data: any) => {
+        if (data.status === 200) {
+          this.categories = data.categories;
+        }
+      },
+      error: (error: any) => {
+        this.showMessage(error?.error?.message || error?.message || "Enable to get categories" + error);
+      }
+    });
+  }
+
+  getProductById(productId: string): void {
+    this.apiService.getProductById(productId).subscribe({
+      next: (data: any) => {
+        if (data.status === 200) {
+          const product = data.product;
+          this.name = product.name;
+          this.sku = product.sku;
+          this.price = product.price;
+          this.stockQuantity = product.stockQuantity;
+          this.description = product.description;
+          this.imageUrl = product.imageUrl;
+          this.categoryId = product.categoryId;
+        } else {
+          this.showMessage(data.message);
+        }
+      },
+      error: (error: any) => {
+        this.showMessage(error?.error?.message || error?.message || "Enable to get product" + error);
+      }
+    });
+  }
+
+  handleImageChange(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input?.files?.length) {
+
+      this.imageFile = input.files[0]
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        this.imageUrl = reader.result as string
+      }
+      reader.readAsDataURL(this.imageFile);
+    }
+  }
+
+  handleSubmit(event: Event): void {
+    event.preventDefault()
+    const formData = new FormData();
+    formData.append('name', this.name);
+    formData.append('sku', this.sku);
+    formData.append('price', this.price);
+    formData.append('stockQuantity', this.stockQuantity);
+    formData.append('categoryId', this.categoryId);
+    formData.append('description', this.description);
+    if (this.imageFile) {
+      formData.append('imageFile', this.imageFile);
+    }
+
+    if (this.isEditing) {
+      formData.append('productId', this.productId!);
+      this.apiService.updateProduct(formData).subscribe({
+        next: (data: any) => {
+          if (data.status === 200) {
+            this.showMessage(data.message);
+            this.router.navigate(['/product']);
+          }
+        },
+        error: (error: any) => {
+          this.showMessage(error?.error?.message || error?.message || "Enable to update product" + error);
+        }
+      })
+    } else {
+      this.apiService.addProduct(formData).subscribe({
+        next: (data: any) => {
+          if (data.status === 200) {
+            this.showMessage(data.message);
+            this.router.navigate(['/product']);
+          }
+        },
+        error: (error: any) => {
+          this.showMessage(error?.error?.message || error?.message || "Enable to add product" + error);
+        }
+      })
+    }
+  }
+
+  showMessage(message: string): void {
+    this.message = message;
+    setTimeout(() => {
+      this.message = '';
+    }, 5000);
+  }
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,12 +1,87 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {ApiService} from "../service/api.service";
+import {Router} from "@angular/router";
+import {PaginationComponent} from "../pagination/pagination.component";
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, PaginationComponent],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+
+  constructor(private apiService: ApiService, private router: Router) {
+  }
+
+  products: any[] = [];
+  message: string = '';
+  currentPage: number = 1;
+  totalPages: number = 0;
+  itemsPerPage: number = 10;
+
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.apiService.getAllProducts().subscribe({
+      next: (data: any) => {
+        if (data.status === 200) {
+          const products = data.products;
+          this.products = data.products || [];
+          this.totalPages = Math.ceil(products.length / this.itemsPerPage);
+          this.products = products.slice(
+            (this.currentPage - 1) * this.itemsPerPage,
+            this.currentPage * this.itemsPerPage);
+        } else {
+          this.showMessage(data.message);
+        }
+      },
+      error: (error: any) => {
+        this.showMessage(error?.error?.message || error?.message || "Enable to get products" + error);
+      }
+    });
+  }
+
+  onPageChange(page: number): void {
+    this.currentPage = page;
+    this.getProducts();
+  }
+
+  handleDeleteProduct(productId: string): void {
+    if (window.confirm("Are you sure you want to delete this product?")) {
+      this.apiService.deleteProduct(productId).subscribe({
+        next: (data: any) => {
+          if (data.status === 200) {
+            this.showMessage("Product deleted successfully");
+            this.getProducts();
+          }
+        },
+        error: (error: any) => {
+          this.showMessage(error?.error?.message || error?.message || "Enable to delete product" + error);
+        }
+      });
+    }
+  }
+
+  navigateToAddProduct(): void {
+    this.router.navigate(['/add-product']);
+  }
+
+  navigateToEditProduct(productId: string): void {
+    this.router.navigate([`/edit-product/${productId}`]);
+  }
+
+  showMessage(message: string): void {
+    this.message = message;
+    setTimeout(() => {
+      this.message = '';
+    }, 3000);
+  }
 
 }
